0fd39e6ad2cdce00ef3d57ce50d3c909657b661f
p1tonidze <p1t0nidze422@gmail.com>
2024-10-12 17:17:18 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,5 +1,3 @@
--- Create database and tables
-
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
@@ -9,25 +7,39 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+CREATE TABLE Warehouses (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    Address VARCHAR(50) NOT NULL,
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
 );
 
--- Populate test data
+CREATE TABLE Products (
+    ID INT PRIMARY KEY,
+    ProductName VARCHAR(50) NOT NULL
+);
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+CREATE TABLE ProductInventory (
+    ProductID INT,
+    WarehouseID INT,
+    Amount INT,
+    PRIMARY KEY (ProductID, WarehouseID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION
+);
+
+INSERT INTO Countries (ID, Name) VALUES
+    (1, 'Country1'),
+    (2, 'Country2');
+
+INSERT INTO Warehouses (ID, Name, Address, CountryID) VALUES
+    (1, 'Warehouse-1', 'City-1, Street-1', 1),
+    (2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+INSERT INTO Products (ID, ProductName) VALUES
+    (1, 'AwesomeProduct');
+
+INSERT INTO ProductInventory (ProductID, WarehouseID, Amount) VALUES
+    (1, 1, 2),
+    (1, 2, 5);
