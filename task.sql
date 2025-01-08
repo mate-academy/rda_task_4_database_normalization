868f5131c32ee37011fc9e3e0261e7a00eaa3e4c
Beliar24 <blackdid7@gmail.com>
2025-01-08 23:30:23 +0200

    normalization db scripts

--- task.sql
+++ task.sql
@@ -4,13 +4,13 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE Warehouses (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     Address VARCHAR(50),
     CountryID INT,
@@ -19,36 +19,36 @@ CREATE TABLE Warehouses (
 );
 
 CREATE TABLE Products (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE ProductInventory (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     ProductID INT,
     WarehouseAmount INT,
     WarehouseID INT,
-	FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
 );
 
 -- Populate test data
 
-INSERT INTO Countries (ID, Name)
-VALUES (1, 'Country1'), (2, 'Country2');
+INSERT INTO Countries (Name)
+VALUES ('Country1'), ('Country2');
 
-INSERT INTO Warehouses (ID, Name, Address, CountryID)
+INSERT INTO Warehouses (Name, Address, CountryID)
 VALUES
-    (1, 'Warehouse-1', 'City-1, Street-1', 1),
-    (2, 'Warehouse-2', 'City-2, Street-2', 2);
+    ('Warehouse-1', 'City-1, Street-1', 1),
+    ('Warehouse-2', 'City-2, Street-2', 2);
 
-INSERT INTO Products (ID, Name)
+INSERT INTO Products (Name)
 VALUES
-    (1, 'AwersomeProduct');
+    ('AwesomeProduct');
 
-INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID)
+INSERT INTO ProductInventory (ProductID, WarehouseAmount, WarehouseID)
 VALUES
-    (1, 1, 2, 1),
-    (2, 1, 5, 2);
+    (1, 2, 1),
+    (1, 5, 2);
