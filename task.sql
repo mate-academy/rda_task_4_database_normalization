bd2b13c366b8971b39d197d09abe79b174b6032b
Yevgene-DP <ebalalay@gmail.com>
2025-06-02 23:30:35 +0300

    Add database Normalization

--- task.sql
+++ task.sql
@@ -1,33 +1,56 @@
--- Create database and tables
-
+DROP DATABASE IF EXISTS ShopDB;
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
+-- 1. Countries
 CREATE TABLE Countries (
-    ID INT,
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+-- 2. Products
+CREATE TABLE Products (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+-- 3. Warehouses
+CREATE TABLE Warehouses (
+    ID INT PRIMARY KEY,
     Name VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    Address VARCHAR(100),
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
 );
 
--- Populate test data
+-- 4. ProductInventory
+CREATE TABLE ProductInventory (
+    ID INT PRIMARY KEY,
+    ProductID INT,
+    WarehouseID INT,
+    Amount INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE CASCADE
+);
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+-- ------------- INSERT TEST DATA -------------
+
+-- Countries
+INSERT INTO Countries (ID, Name) VALUES (1, 'Country1');
+INSERT INTO Countries (ID, Name) VALUES (2, 'Country2');
+
+-- Products
+INSERT INTO Products (ID, Name) VALUES (1, 'AwersomeProduct');
+
+-- Warehouses
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
+VALUES 
+    (1, 'Warehouse-1', 'City-1, Street-1', 1),
+    (2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+-- Product Inventory
+INSERT INTO ProductInventory (ID, ProductID, WarehouseID, Amount)
+VALUES 
+    (1, 1, 1, 2),
+    (2, 1, 2, 5);
+
