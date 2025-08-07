9ba01c48ef8089cef97b44c5c535864d6411f689
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2025-08-07 19:43:18 +0300

    solution

--- task.sql
+++ task.sql
@@ -1,5 +1,3 @@
--- Create database and tables
-
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
@@ -9,25 +7,45 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
+CREATE TABLE Warehouses(
+	ID INT,
+    Name VARCHAR(50),
+    Address VARCHAR(50), 
+	CountryID INT,
 	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
--- Populate test data
+CREATE TABLE Products(
+	ID INT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
+);
 
-INSERT INTO Countries (ID,Name)
+CREATE TABLE ProductInventory (
+    ID INT,
+    WarehouseAmount INT,
+    ProductID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO Countries (ID, Name)
 	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
+INSERT INTO Countries (ID, Name)
 	VALUES (2, 'Country2');
+
+INSERT INTO Warehouses(ID, Name, Address, CountryID)
+	VALUES (1, 'Warehouse-1', 'City-1, Street-1',1);
+INSERT INTO Warehouses(ID, Name, Address, CountryID)
+	VALUES (2, 'Warehouse-2', 'City-2, Street-2',2);
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Products (ID, Name)
+	VALUES (1, 'AwersomeProduct');
+
+INSERT INTO ProductInventory (ID, WarehouseAmount, ProductID, WarehouseID)
+	VALUES (1, 2, 1, 1);
+INSERT INTO ProductInventory (ID, WarehouseAmount, ProductID, WarehouseID)
+	VALUES (2, 5, 1, 2);
