8f327f6498f9ba831e57d08d46700056b0697d61
Ivan <ivanqqq@ukr.net>
2026-03-12 15:50:31 +0100

    fix comments

--- task.sql
+++ task.sql
@@ -18,12 +18,20 @@ CREATE TABLE Warehouses (
 	    ON UPDATE CASCADE
 );
 
-CREATE TABLE ProductInventory (
+CREATE TABLE Products (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ProductName VARCHAR(50),
+);
+
+CREATE TABLE ProductInventory (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ProductId INT,
     WarehouseId INT,
     WarehouseAmount INT,
     FOREIGN KEY (WarehouseId) REFERENCES Warehouses(ID)
+    	ON DELETE NO ACTION
+	    ON UPDATE CASCADE
+	FOREIGN KEY (ProductId) REFERENCES Products(ID)
     	ON DELETE NO ACTION
 	    ON UPDATE CASCADE
 );
@@ -37,8 +45,11 @@ INSERT INTO Countries (ID,Name)
 
 INSERT INTO Warehouses (WarehouseName,WarehouseAddress,CountryID)
     VALUES ('Warehouse-1', 'City-1, Street-1',1), ('Warehouse-2', 'City-2, Street-2',2);
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount)
-	VALUES (1, 'AwersomeProduct', 1, 2);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount)
-	VALUES (2, 'AwersomeProduct', 2, 5);
+
+INSERT INTO Products (ID,ProductName)
+    VALUES (1, 'AwersomeProduct'), (2, 'AwersomeProduct');
+
+INSERT INTO ProductInventory (ProductId,WarehouseAmount)
+	VALUES (1, 2);
+INSERT INTO ProductInventory (ProductId,WarehouseAmount)
+	VALUES (2, 5);
