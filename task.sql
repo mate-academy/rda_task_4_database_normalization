236b078ff19c9ae73173d6649f81ba74b76c8f6a
Ivan <ivanqqq@ukr.net>
2026-03-12 15:31:04 +0100

    improved till 3rd NF

--- task.sql
+++ task.sql
@@ -4,20 +4,28 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Warehouses (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50),
+    CountryID INT,
+	FOREIGN KEY (CountryID) REFERENCES Countries(ID)
+	    ON DELETE NO ACTION
+	    ON UPDATE CASCADE
 );
 
 CREATE TABLE ProductInventory (
-    ID INT,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     ProductName VARCHAR(50),
+    WarehouseId INT,
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    FOREIGN KEY WarehouseId REFERENCES Warehouses(ID)
+    	ON DELETE NO ACTION
+	    ON UPDATE CASCADE
 );
 
 -- Populate test data
@@ -26,8 +34,11 @@ INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
+
+INSERT INTO Warehouses (WarehouseName,WarehouseAddress,CountryID)
+    VALUES ('Warehouse-1', 'City-1, Street-1',1), ('Warehouse-2', 'City-2, Street-2',2);
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount)
+	VALUES (1, 'AwersomeProduct', 2);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount)
+	VALUES (2, 'AwersomeProduct', 5);
