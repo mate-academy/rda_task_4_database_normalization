afb1d5f0f4cc65baacc981e5ff34142dedbec400
dimonalek <dimonalek@gmail.com>
2026-04-20 14:12:45 +0300

    DB Normalised

--- task.sql
+++ task.sql
@@ -1,21 +1,11 @@
 -- Create database and tables
-
+DROP DATABASE IF EXISTS ShopDB;
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductID INT,
-    WarehouseAmount INT,
-    WarehouseID INT,
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -35,6 +25,18 @@ CREATE TABLE Products (
     Price INT,
     PRIMARY KEY (ID)
 );
+
+CREATE TABLE ProductInventory (
+    ID INT,
+    ProductID INT,
+    WarehouseAmount INT,
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
+
 -- Populate test data
 
 INSERT INTO Countries (ID,Name)
@@ -46,3 +48,13 @@ INSERT INTO ProductInventory (ID,ProductID,WarehouseAmount,WarehouseID)
 	VALUES (1, 1, 2, 1);
 INSERT INTO ProductInventory (ID,ProductID,WarehouseAmount,WarehouseID)
 	VALUES (2, 1, 5, 2);
+
+INSERT INTO Products (ID,Name,Description,Price)
+    VALUES (1, 'Product1', 'Description1', 100);
+INSERT INTO Products (ID,Name,Description,Price)
+    VALUES (2, 'Product2', 'Description2', 200);    
+
+INSERT INTO Warehouses (ID,Name,Address,CountryID)
+    VALUES (1, 'Warehouse1', 'Address1', 1);
+INSERT INTO Warehouses (ID,Name,Address,CountryID)
+    VALUES (2, 'Warehouse2', 'Address2', 2);
