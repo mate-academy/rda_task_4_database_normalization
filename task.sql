1e0387ac7d2d7b9512318b537514f944d881cb47
Ihor Hurianov <igor.gurianov@gmail.com>
2026-05-25 19:07:37 +0300

    Solution

--- task.sql
+++ task.sql
@@ -12,16 +12,16 @@ CREATE TABLE Countries (
 CREATE TABLE Warehouses (
     ID INT,
     Name VARCHAR(50),
-    Adress VARCHAR(50),
+    Address VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY CountryID REFERENCES Countries(ID),
+    CONSTRAINT fk_warehouse_country  FOREIGN KEY (CountryID) REFERENCES Countries(ID),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE Products (
     ID INT,
     Name VARCHAR(50),
-    PRIMARY KEY (ID),
+    PRIMARY KEY (ID)
 );
 
 CREATE TABLE ProductInventory (
@@ -29,8 +29,8 @@ CREATE TABLE ProductInventory (
     ProductID INT,
     WarehouseID INT,
     Amount INT,
-    FOREIGN KEY (ProductID) REFERENCES Products (ID),
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses (ID),
+    CONSTRAINT fk_inventory_product FOREIGN KEY (ProductID) REFERENCES Products (ID),
+    CONSTRAINT fk_inventory_warehouse FOREIGN KEY (WarehouseID) REFERENCES Warehouses (ID),
     PRIMARY KEY (ID)
 );
 
@@ -41,7 +41,7 @@ INSERT INTO Countries (ID,Name)
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
 
- INSERT INTO Warehouses (ID, Name, Adress, CountryID) 
+ INSERT INTO Warehouses (ID, Name, Address, CountryID) 
   VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
 INSERT INTO Warehouses (ID, Name, Address, CountryID)
   VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
