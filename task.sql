ef0461cb521bb9b9965758b100fe498d085c4492
Petro Kravchenko <petros5325@gmail.com>
2026-06-05 00:42:30 +0300

    Solution

--- task.sql
+++ task.sql
@@ -8,15 +8,35 @@ CREATE TABLE Countries (
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
+CREATE TABLE Cities (
+    ID INT AUTO_INCREMENT,
+    NAME VARCHAR(100),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Streets (
+    ID INT AUTO_INCREMENT,
+    NAME VARCHAR(100),
+    CityID INT,
+    FOREIGN KEY (CityID) REFERENCES Cities(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Warehouses (
+    ID INT AUTO_INCREMENT,
+    NAME VARCHAR(100),
+    StreetID INT,
+    FOREIGN KEY (StreetID) REFERENCES Streets(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
 
 CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
+    ID INT AUTO_INCREMENT, 
+    ProductName VARCHAR(100),
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -26,8 +46,23 @@ INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
+
+INSERT INTO Cities (NAME,CountryID)
+    VALUES ('City-1', 1);
+INSERT INTO Cities (NAME,CountryID)
+    VALUES ('City-2', 2);
+
+INSERT Streets (NAME,CityID)
+    VALUES ('Street-1', 1);
+INSERT Streets (NAME,CityID)
+    VALUES ('Street-2', 2);
+
+INSERT INTO Warehouses (NAME,StreetID)
+    VALUES ('Warehouse-1', 1);
+INSERT INTO Warehouses (NAME,StreetID)
+    VALUES ('Warehouse-2', 2);
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseID)
+	VALUES (1, 'AwersomeProduct', 2, 1);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseID)
+	VALUES (2, 'AwersomeProduct', 5, 2);
