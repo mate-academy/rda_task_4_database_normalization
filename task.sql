7b568c0f630d98bed4dfbc3155955ff1cdf6a4c9
vadym <vadym.rurka01@gmail.com>
2026-07-16 23:02:13 +0200

    new solution

--- task.sql
+++ task.sql
@@ -8,13 +8,11 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
--- 1. Створюємо таблицю для товарів
 CREATE TABLE Products (
     ID INT PRIMARY KEY,
     ProductName VARCHAR(50)
 );
 
--- 2. Створюємо таблицю для складів
 CREATE TABLE Warehouses (
     ID INT PRIMARY KEY,
     WarehouseName VARCHAR(50),
@@ -23,7 +21,6 @@ CREATE TABLE Warehouses (
     FOREIGN KEY (CountryID) REFERENCES Countries(ID)
 );
 
--- 3. Створюємо таблицю-зв'язковий для інвентарю
 CREATE TABLE ProductInventory (
     ID INT PRIMARY KEY,
     WarehouseAmount INT,
@@ -40,17 +37,14 @@ INSERT INTO Countries (ID, Name)
 INSERT INTO Countries (ID, Name)
     VALUES (2, 'Country2');
 
--- Заповнюємо товар
 INSERT INTO Products (ID, ProductName)
     VALUES (1, 'AwersomeProduct');
 
--- Заповнюємо склади
 INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
     VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
 INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
     VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
 
--- Заповнюємо кількість товарів на конкретних складах
 INSERT INTO ProductInventory (ID, WarehouseAmount, ProductID, WarehouseID)
     VALUES (1, 2, 1, 1);
 INSERT INTO ProductInventory (ID, WarehouseAmount, ProductID, WarehouseID)
