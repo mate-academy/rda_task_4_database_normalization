bedfa9b9e00c6b22be26cfd97d67614fba5ef5b8
Illia Fedoruk <fedorukstar@gmail.com>
2026-07-16 16:58:15 +0300

    Normalize ShopDB database to 3NF

--- task.sql
+++ task.sql
@@ -8,16 +8,27 @@ CREATE TABLE Countries (
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
-
-CREATE TABLE ProductInventory (
+CREATE TABLE Products (
     ID INT,
     ProductName VARCHAR(50),
-    WarehouseAmount INT,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Warehouses (
+    ID INT,
     WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    WarehouseAddress VARCHAR(50),
     CountryID INT,
 	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
+);
+CREATE TABLE ProductInventory (
+    ID INT,
+    WarehouseAmount INT,
+    ProductID INT,
+	FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    WarehouseID INT,
+	FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+	PRIMARY KEY (ID)
 );
 
 -- Populate test data
@@ -27,7 +38,14 @@ INSERT INTO Countries (ID,Name)
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Products (ID,ProductName)
+	VALUES (1, 'AwersomeProduct');
+INSERT INTO Warehouses (ID,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (1, 'Warehouse-1', 'City-1, Street-1',1);
+INSERT INTO ProductInventory (ID,WarehouseAmount,ProductID,WarehouseID)
+	VALUES (1, 2, 1, 1);
+
+INSERT INTO Warehouses (ID,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (2, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,WarehouseAmount,ProductID,WarehouseID)
+	VALUES (2, 5, 1, 2);
