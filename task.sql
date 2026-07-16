cc95293fa45ba8432082dd31980444ce8a9f6bb8
vadym <vadym.rurka01@gmail.com>
2026-07-16 23:00:57 +0200

    solution

--- task.sql
+++ task.sql
@@ -1,5 +1,4 @@
 -- Create database and tables
-
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
@@ -9,25 +8,50 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
-    ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
+-- 1. Створюємо таблицю для товарів
+CREATE TABLE Products (
+    ID INT PRIMARY KEY,
+    ProductName VARCHAR(50)
+);
+
+-- 2. Створюємо таблицю для складів
+CREATE TABLE Warehouses (
+    ID INT PRIMARY KEY,
     WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    WarehouseAddress VARCHAR(50),
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
+);
+
+-- 3. Створюємо таблицю-зв'язковий для інвентарю
+CREATE TABLE ProductInventory (
+    ID INT PRIMARY KEY,
+    WarehouseAmount INT,
+    ProductID INT,
+    WarehouseID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
 );
 
 -- Populate test data
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Countries (ID, Name)
+    VALUES (1, 'Country1');
+INSERT INTO Countries (ID, Name)
+    VALUES (2, 'Country2');
+
+-- Заповнюємо товар
+INSERT INTO Products (ID, ProductName)
+    VALUES (1, 'AwersomeProduct');
+
+-- Заповнюємо склади
+INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
+INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
+    VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+-- Заповнюємо кількість товарів на конкретних складах
+INSERT INTO ProductInventory (ID, WarehouseAmount, ProductID, WarehouseID)
+    VALUES (1, 2, 1, 1);
+INSERT INTO ProductInventory (ID, WarehouseAmount, ProductID, WarehouseID)
+    VALUES (2, 5, 1, 2);
