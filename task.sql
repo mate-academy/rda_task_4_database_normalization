7d735192c2d7971b07cff0e17ac09a358a0d142d
Diana Khmeliuk <dianakhmeluyk@gmail.com>
2026-08-02 11:58:21 +0300

    'Solution'

--- task.sql
+++ task.sql
@@ -12,13 +12,19 @@ CREATE TABLE Countries (
 CREATE TABLE ProductInventory (
     ID INT,
     ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    WarehouseID INT,
     CountryID INT,
 	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+	FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
+
+CREATE TABLE Warehouse(
+    ID INT,
+    WarehouseAmount INT,
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50),
+)
 
 -- Populate test data
 
@@ -27,7 +33,12 @@ INSERT INTO Countries (ID,Name)
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,ProductName,CountryID)
+	VALUES (1, 'AwersomeProduct', 2 ,1);
+INSERT INTO ProductInventory (ID,ProductName,CountryID)
+	VALUES (2, 'AwersomeProduct', 2);
+
+INSERT INTO Warehouse (ID,WarehouseAmount,WarehouseName,WarehouseAddress)
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1')
+INSERT INTO Warehouse (ID,WarehouseAmount,WarehouseName,WarehouseAddress)
+    VALUES (2, 5, 'Warehouse-2', 'City-2, Street-2')
