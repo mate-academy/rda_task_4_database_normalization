7e089cb2e4ab6b2fb2c5f2e5f0bb2ae3063c556e
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-09-02 14:25:35 +0300

    Fix according test

--- task.sql
+++ task.sql
@@ -3,6 +3,7 @@ DROP DATABASE IF EXISTS ShopDB;
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
+
 CREATE TABLE Countries
 (
     ID   INT AUTO_INCREMENT,
@@ -10,53 +11,55 @@ CREATE TABLE Countries
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE Cities
-(
-    ID   INT AUTO_INCREMENT,
-    CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES Countries (ID) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-CREATE INDEX CountryID ON Cities (CountryID);
-
 CREATE TABLE Warehouses
 (
-    ID        INT AUTO_INCREMENT,
-    CityID INT,
-    FOREIGN KEY (CityID) REFERENCES Cities (ID) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    WarehouseAddress   VARCHAR(50),
+    ID               INT AUTO_INCREMENT,
+    CountryID        INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries (ID) ON UPDATE NO ACTION ON DELETE NO ACTION,
+    WarehouseAddress VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
-CREATE INDEX CityID ON Warehouses (CityID);
+CREATE INDEX CountryID ON Warehouses (CountryID);
+
+
+CREATE TABLE Products
+(
+    ID          INT AUTO_INCREMENT,
+    ProductName VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
 
 CREATE TABLE ProductInventory
 (
     ID              INT AUTO_INCREMENT,
-    ProductName     VARCHAR(50),
+    ProductID       INT,
+    FOREIGN KEY (ProductID) REFERENCES Products (ID) ON UPDATE NO ACTION ON DELETE NO ACTION,
     WarehouseAmount INT,
     WarehouseID     INT,
     FOREIGN KEY (WarehouseID) REFERENCES Warehouses (ID) ON UPDATE NO ACTION ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
-CREATE INDEX Name ON ProductInventory (WarehouseID);
+CREATE INDEX ProductID ON ProductInventory (ProductID);
+CREATE INDEX WarehouseID ON ProductInventory (WarehouseID);
+
+-- ---------------- INSERTED DATA -----------------
 
 -- Countries
-INSERT INTO Countries (ID, Name) VALUES (1, 'Country1');
-INSERT INTO Countries (ID, Name) VALUES (2, 'Country2');
-
--- Cities
-INSERT INTO Cities (ID, CountryID, Name) VALUES (1, 1, 'City-1');
-INSERT INTO Cities (ID, CountryID, Name) VALUES (2, 2, 'City-2');
+INSERT INTO Countries (ID, Name)
+VALUES (1, 'Country1');
+INSERT INTO Countries (ID, Name)
+VALUES (2, 'Country2');
 
 -- Warehouses
-INSERT INTO Warehouses (ID, CityID, WarehouseAddress) VALUES (1, 1, 'Street-1');
-INSERT INTO Warehouses (ID, CityID, WarehouseAddress) VALUES (2, 2, 'Street-2');
+INSERT INTO Warehouses (ID, CountryID, WarehouseAddress) VALUES (1, 1, "City-1, Street-1");
+INSERT INTO Warehouses (ID, CountryID, WarehouseAddress) VALUES (2, 2, "City-2, Street-2");
 
 -- Products
-INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
-VALUES (1, 'AwersomeProduct', 2, 1);
-INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
-VALUES (2, 'AwersomeProduct', 5, 2);
+INSERT INTO Products (ID, ProductName) VALUES (1, "AwersomeProduct");
+
+-- Products Inventory
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID) VALUES (1, 1, 2, 1);
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID) VALUES (2, 1, 5, 2);
